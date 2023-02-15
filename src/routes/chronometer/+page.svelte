7eb6f44d68ca3673dbdb71f37ced709dc3728565
Warbacon <joaquinguerratocino@gmail.com>
2023-02-15 09:44:26 +0100

    Clear sessionStorage

--- src/routes/chronometer/+page.svelte
+++ src/routes/chronometer/+page.svelte
@@ -38,7 +38,7 @@
 		hours = parseInt(time.substring(0, 2));
 		minutes = parseInt(time.substring(3, 5));
 		seconds = parseInt(time.substring(6));
-		sessionStorage.setItem('time', time);
+		sessionStorage.removeItem('time');
 	}
 
 	function toggleChronometer() {
