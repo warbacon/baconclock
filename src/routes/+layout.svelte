5e9dd671d89c1eeecf5fe68dbff7b4ca85e2e4a1
Warbacon <joaquinguerratocino@gmail.com>
2023-12-21 14:41:19 +0100

    Remove vercel speedinsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import './styles.css';
 	import { page } from '$app/stores';
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
-	injectSpeedInsights();
 </script>
 
 <div>
