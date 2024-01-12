ac6cac043b66723ec861b88ba4dba4c753adc4a7
Warbacon <joaquinguerratocino@gmail.com>
2024-01-12 16:57:58 +0100

    Better time display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import { onDestroy, onMount } from 'svelte';
 	let date = new Date();
 	let seconds: string = '00';
@@ -7,19 +6,17 @@
 	let hours: string = '00';
 	let timeInterval: number;
 
-	if (browser) {
-		onMount(() => {
-			timeInterval = setInterval(() => {
-				date = new Date();
-			}, 250);
-		});
-	}
+	onMount(() => {
+		timeInterval = setInterval(() => {
+			date = new Date();
+		}, 250);
+	});
 	onDestroy(() => clearInterval(timeInterval));
 
 	$: {
 		seconds = String(date.getSeconds()).padStart(2, '0');
 		minutes = String(date.getMinutes()).padStart(2, '0');
-		hours = String(date.getHours()).padStart(2, '0');
+		hours = String(date.getHours());
 	}
 </script>
 
